e67bbcefed14634fd38baa31d2106638879d4cba
ArhabSafwan <asafwan72@gmail.com>
2025-08-24 22:27:14 +0600

    changed footer

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -1,12 +1,12 @@
-
-
 <!-- Footer -->
 <footer class="content-footer footer bg-footer-theme">
     <div class="container-xxl">
         <div class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column">
             <div>
-                <span class="custom-number"></span> &nbsp;<span class="custom-number">©2025 BRAC Innovation, All Rights Reserved | Built with</span> <i class="fa fa-heart text-danger"></i> By <a class="custom-number" href="https://3-devs.com/">3DEVs IT Ltd.</a>
+                <span class="custom-number"></span> &nbsp;<span class="custom-number">©2025 Webloom, All Rights Reserved
+                    | Built with</span> <i class="fa fa-heart text-danger"></i> By <a class="custom-number"
+                    href="https://arhab-portfolio.webloomst.net//">Arhab Safwan</a>
+            </div>
         </div>
-    </div>
 </footer>
 <!-- / Footer -->
