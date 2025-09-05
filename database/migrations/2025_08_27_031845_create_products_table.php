c084f4af3d915740914a1235b1d80ef843f4105a
ArhabSafwan <asafwan72@gmail.com>
2025-09-05 21:29:42 +0600

    remove the unique product title

--- database/migrations/2025_08_27_031845_create_products_table.php
+++ database/migrations/2025_08_27_031845_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
-            $table->string('slug')->unique()->nullable();
+            $table->string('slug')->nullable();
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2)->nullable();
             $table->integer('stock')->default(0)->nullable();
